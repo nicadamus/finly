4343d2b67644ecca55176ebb74075b08a7ee2eb8
Nicholas Kefaloukos <n_kefaloukos@hotmail.com>
2025-03-17 17:48:08 +1100

    Added Morgan for dev logging

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.get('/',(req,res) =>{
     res.send('Hello from Node.js');
